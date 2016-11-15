a18c6358fd251875f902a5aaa95567948ad3a3a7
Jacques van Wyk <jvw679@gmail.com>
2016-11-15 21:00:11 +0200

    Initial

---
+++ about.php
@@ -0,0 +1,19 @@
+<?php $page = 'about';include 'include\header.php'; ?>
+<hr>
+    <div class="container">
+
+      <div class="starter-template">
+        <h1>Our Aboutpage</h1>
+      </div>
+
+    </div><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
